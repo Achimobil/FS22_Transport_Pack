c06cf493a9c65830149d199adc340a1355abaac4
Achim Kuntz <achimobil@hotmail.com>
2024-01-18 14:05:57 +0100

    cmd

---
+++ Test.Galgenberg.2.cmd
@@ -0,0 +1,2 @@
+"C:\Program Files\7-Zip\7z.exe" u -tzip "E:\LS22Profile\Galgenberg\mods\FS22_Transport_Pack.zip" -x@FileDefinition.txt
+start "" "C:\Program Files (x86)\Farming Simulator 2022\x64\FarmingSimulator2022Game.exe" -autoStartSavegameId 2 -cheats -profile E:\LS22Profile\Galgenberg -devWarning
